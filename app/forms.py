1a2b7b697fdb6a4b1daafd9adbc3543149733957
Manioco <prausartudo@gmail.com>
2024-07-03 21:54:07 -0300

    antes do migrate

--- app/forms.py
+++ app/forms.py
@@ -1,40 +1,90 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, NumberRange, Optional, Regexp, Length
+from wtforms import (DateField, IntegerField, PasswordField, StringField,
+                     SubmitField, TextAreaField)
+from wtforms.validators import (DataRequired, InputRequired, Length,
+                                NumberRange, Optional, Regexp, ValidationError)
+from models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=3, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if not existing_user_username:
+            raise ValidationError("Username does not exist!")
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=3, max=20), Regexp(
+        "^[a-z]+$", message="Username must contain only lowercase letters")], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "Username already exists! Please try another one.")
 
 
 class LivroForm(FlaskForm):
     nomeLivro = StringField('Nome do Livro', validators=[DataRequired()])
-    quantidade = IntegerField('Quantidade', default=1, validators=[DataRequired(), NumberRange(min=1)])
+    quantidade = IntegerField('Quantidade', default=1, validators=[
+                              DataRequired(), NumberRange(min=1)])
     nomeAutor = StringField('Nome do Autor', validators=[DataRequired()])
     nomeEditora = StringField('Nome da Editora', validators=[DataRequired()])
-    dataPublicacao = DateField('Data de Publicação', format='%Y-%m-%d', validators=[DataRequired()])
-    dtAquisicao = DateField('Data de Aquisição', format='%Y-%m-%d', validators=[DataRequired()])
+    dataPublicacao = DateField(
+        'Data de Publicação', format='%Y-%m-%d', validators=[DataRequired()])
+    dtAquisicao = DateField('Data de Aquisição',
+                            format='%Y-%m-%d', validators=[DataRequired()])
     descricao = TextAreaField('Descrição', validators=[Optional()])
     submit = SubmitField('Cadastrar')
 
 
 class AlunoForm(FlaskForm):
     nomeAluno = StringField('Nome do Aluno', validators=[DataRequired()])
-    telefoneAluno = StringField('Telefone do Aluno', validators=[Optional(), Length(max=12), Regexp(r'^\d+$', message="Telefone deve conter apenas números")])
-    dtNascimento = DateField('Data de Nascimento', format='%Y-%m-%d', validators=[DataRequired()])
-    cpf = StringField('CPF', validators=[Optional(), Length(min=11,max=11), Regexp(r'^\d+$', message="CPF deve conter apenas números")])
-    rg = StringField('RG', validators=[Optional(), Length(min=9,max=10), Regexp(r'^\d+$', message="RG deve conter apenas números")])
-    serieTurma = StringField('Série/Turma', validators=[DataRequired()])
-    nomeResponsavel1 = StringField('Nome do Responsável 1', validators=[Optional()])
-    telefoneResponsavel1 = StringField('Telefone do Responsável 1', validators=[Optional()])
-    nomeResponsavel2 = StringField('Nome do Responsável 2', validators=[Optional()])
-    telefoneResponsavel2 = StringField('Telefone do Responsável 2', validators=[Optional()])
+    telefoneAluno = StringField('Telefone do Aluno', validators=[Optional(), Length(
+        max=12), Regexp(r'^\d+$', message="Telefone deve conter apenas números")])
+    dtNascimento = DateField('Data de Nascimento',
+                             format='%Y-%m-%d', validators=[DataRequired()])
+    cpf = StringField('CPF', validators=[Optional(), Length(
+        min=11, max=11), Regexp(r'^\d+$', message="CPF deve conter apenas números")])
+    rg = StringField('RG', validators=[Optional(), Length(min=9, max=10), Regexp(
+        r'^\d+$', message="RG deve conter apenas números")])
+    serie = StringField('Série', validators=[DataRequired()])
+    turma = StringField('Turma', validators=[Optional()])
+    nomeResponsavel1 = StringField(
+        'Nome do Responsável 1', validators=[Optional()])
+    telefoneResponsavel1 = StringField(
+        'Telefone do Responsável 1', validators=[Optional()])
+    nomeResponsavel2 = StringField(
+        'Nome do Responsável 2', validators=[Optional()])
+    telefoneResponsavel2 = StringField(
+        'Telefone do Responsável 2', validators=[Optional()])
     observacao = TextAreaField('Observação', validators=[Optional()])
     submit = SubmitField('Cadastrar')
 
 
 class EmprestimoForm(FlaskForm):
-    quantidade = IntegerField('Quantidade', validators=[DataRequired(), NumberRange(min=1)])
-    dataEmprestimo = DateField('Data de Empréstimo', format='%Y-%m-%d', validators=[DataRequired()])
-    dataDevolucao = DateField('Data de Devolução', format='%Y-%m-%d', validators=[DataRequired()])
-    aluno_id = IntegerField('ID do Aluno', validators=[DataRequired(), NumberRange(min=1)])
-    livro_id = IntegerField('ID do Livro', validators=[DataRequired(), NumberRange(min=1)])
+    quantidade = IntegerField('Quantidade', validators=[
+                              DataRequired(), NumberRange(min=1)])
+    dataEmprestimo = DateField(
+        'Data de Empréstimo', format='%Y-%m-%d', validators=[DataRequired()])
+    dataDevolucao = DateField(
+        'Data de Devolução', format='%Y-%m-%d', validators=[DataRequired()])
+    aluno_id = IntegerField('ID do Aluno', validators=[
+                            DataRequired(), NumberRange(min=1)])
+    livro_id = IntegerField('ID do Livro', validators=[
+                            DataRequired(), NumberRange(min=1)])
     submit = SubmitField('Cadastrar')
 
 
@@ -42,4 +92,3 @@ class PalavraChaveForm(FlaskForm):
     palavra = StringField('Palavra', validators=[DataRequired()])
     livro_id = IntegerField('ID do Livro', validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
-
--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
+from flask_login import UserMixin
+
 from app import db
 
 
-class Livro(db.Model):
-    __tablename__ = 'livro'
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    usertype = db.Column(db.String(80), nullable=False)
+    # Time format: dd/mm/yyyy|hh:mm:ss
+    creationdate = db.Column(db.String(80), nullable=False)
+    lastUpdateDt = db.Column(db.Date, nullable=False)
+    createdBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+    updatedBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+
+
+
+class Livro(db.Model):
+    __tablename__ = 'livros'
+    
     idLivro = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nomeLivro = db.Column(db.String, nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
@@ -12,10 +29,14 @@ class Livro(db.Model):
     dataPublicacao = db.Column(db.Date, nullable=False)
     dtAquisicao = db.Column(db.Date, nullable=True)
     descricao = db.Column(db.Text, nullable=True)
+    creationDt = db.Column(db.Date, nullable=False)
+    lastUpdateDt = db.Column(db.Date, nullable=False)
+    createdBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+    updatedBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
 
 
 class Aluno(db.Model):
-    __tablename__ = 'aluno'
+    __tablename__ = 'alunos'
 
     idAluno = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nomeAluno = db.Column(db.String, nullable=False)
@@ -23,25 +44,34 @@ class Aluno(db.Model):
     dtNascimento = db.Column(db.Date, nullable=False)
     cpf = db.Column(db.String(11), nullable=True)
     rg = db.Column(db.String(10), nullable=True)
-    serieTurma = db.Column(db.String, nullable=False)
+    serie = db.Column(db.Integer, nullable=False)
+    turma = db.Column(db.String, nullable=True)
     nomeResponsavel1 = db.Column(db.String, nullable=True)
     telefoneResponsavel1 = db.Column(db.String, nullable=True)
     nomeResponsavel2 = db.Column(db.String, nullable=True)
     telefoneResponsavel2 = db.Column(db.String, nullable=True)
     observacao = db.Column(db.Text, nullable=True)
+    creationDt = db.Column(db.Date, nullable=False)
+    lastUpdateDt = db.Column(db.Date, nullable=False)
+    createdBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+    updatedBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
 
 
 class PalavraChave(db.Model):
-    __tablename__ = 'palavraChave'
+    __tablename__ = 'palavrasChave'
 
     idPalavra = db.Column(db.Integer, primary_key=True, autoincrement=True)
     palavra = db.Column(db.String, nullable=False)
     livro_id = db.Column(db.Integer, db.ForeignKey('livro.idLivro'), nullable=False)
     livro = db.relationship('Livro', backref=db.backref('palavras_chave', lazy=True))
+    creationDt = db.Column(db.Date, nullable=False)
+    lastUpdateDt = db.Column(db.Date, nullable=False)
+    createdBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+    updatedBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
 
 
 class Emprestimo(db.Model):
-    __tablename__ = 'emprestimo'
+    __tablename__ = 'emprestimos'
 
     idEmprestimo = db.Column(db.Integer, primary_key=True, autoincrement=True)
     quantidade = db.Column(db.Integer, nullable=False)
@@ -51,4 +81,8 @@ class Emprestimo(db.Model):
     livro_id = db.Column(db.Integer, db.ForeignKey('livro.idLivro'), nullable=False)
     aluno = db.relationship('Aluno', backref=db.backref('emprestimos', lazy=True))
     livro = db.relationship('Livro', backref=db.backref('emprestimos', lazy=True))
+    creationDt = db.Column(db.Date, nullable=False)
+    lastUpdateDt = db.Column(db.Date, nullable=False)
+    createdBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
+    updatedBy = db.Column(db.Integer, db.ForeingKey('users.id'), nullable=False)
     
