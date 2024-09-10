db383f9886d04934c63c90bd55ad14671143ebbe
Okapi-LeonardoWorm <prausartudo@gmail.com>
2024-09-09 22:01:37 -0300

    adicionada configuração para o CSRF

--- config.py
+++ config.py
@@ -7,8 +7,11 @@ class Config:
 
     # Configuração para o caminho dos templates
     # TEMPLATE_FOLDER = 'app/templates
-
+    
     # Configuração para a chave secreta
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave_secreta'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///biblioteca.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    WTF_CSRF_ENABLED = True  # Ativa a proteção CSRF
+    WTF_CSRF_METHODS = ['POST', 'PUT', 'DELETE']  # O CSRF será aplicado apenas a esses métodos
