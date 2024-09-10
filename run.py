28c0f0785ec7bd6dd1127dc8669cc201b3a3e0d7
Okapi-LeonardoWorm <prausartudo@gmail.com>
2024-09-09 22:02:11 -0300

    modificado para o conceito de factory junto com o __init__

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
-from app import app
+from app import createApp
 import logging
 from gevent.pywsgi import WSGIServer
 
 
+# Criar a aplicação
+app = createApp()
+
+# Configurar os LOGs de uso
 logging.basicConfig(filename="app.log", level=logging.DEBUG)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Cria o servidor WSGI com gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
+    
+    # Executa o servidor WSGI
+    http_server.serve_forever()
