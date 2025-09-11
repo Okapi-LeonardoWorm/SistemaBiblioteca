6c0486941500c245cc86895e91a7f4b0b540298e
prausartudo <prausartudo@gmail.com>
2025-09-11 20:11:20 +0000

    fix tests

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
 
 class Config:
-    # Configuração para o caminho dos arquivos estáticos
-    # STATIC_FOLDER = 'app/static'
-    # STATIC_URL_PATH = 'app/static'
-
-    # Configuração para o caminho dos templates
-    # TEMPLATE_FOLDER = 'app/templates
-    
-    # Configuração para a chave secreta
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave_secreta'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///biblioteca.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
+    WTF_CSRF_METHODS = ['POST', 'PUT', 'DELETE']
 
-    WTF_CSRF_ENABLED = True  # Ativa a proteção CSRF
-    WTF_CSRF_METHODS = ['POST', 'PUT', 'DELETE']  # O CSRF será aplicado apenas a esses métodos
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory SQLite database for tests
+    WTF_CSRF_ENABLED = False  # Disable CSRF forms protection in tests for simplicity
